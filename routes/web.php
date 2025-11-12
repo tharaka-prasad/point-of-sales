16cfbb46d29d9df3b2940dafe83556cfdca7e452
princenuwan <nuwanuk3@gmail.com>
2025-11-12 11:04:34 +0530

    fixing not session ending problem while login

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ReportsController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KitchenController;
 
 // Login
 Route::get("/", fn() => redirect()->route("login"));
@@ -26,8 +27,19 @@ Route::get("/", fn() => redirect()->route("login"));
 //Logout
 Route::post('/logout', function () {
     Auth::logout();
-    return redirect('/login');
+
+    // Invalidate session and regenerate token
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    // Redirect to login with a full reload
+    return redirect('/login')->withHeaders([
+        'Cache-Control' => 'no-cache, no-store, must-revalidate',
+        'Pragma' => 'no-cache',
+        'Expires' => '0',
+    ]);
 })->name('logout');
+
 
 Route::middleware([
     'auth:sanctum',
@@ -43,25 +55,26 @@ Route::middleware([
 
     Route::middleware(['level:1'])->group(function (){
         // Category
-        Route::get('/category/data', [CategoryController::class, "data"])->name("category.data");
         Route::resource('/category', CategoryController::class);
+        Route::get('/category/data', [CategoryController::class, "data"])->name("category.data");
+
 
         // Product
-        Route::get('/product/data', [ProductController::class, "data"])->name("product.data");
         Route::resource('/product', ProductController::class);
+        Route::get('/product/data', [ProductController::class, "data"])->name("product.data");
         Route::post('/product/delete-selected', [ProductController::class, "deleteSelected"])->name("product.deleteSelected");
         Route::post('/product/print-barcode', [ProductController::class, "printBarcode"])->name("product.printBarcode");
         Route::get('/product/autocode', [ProductController::class, 'autoCode'])->name('product.autocode');
 
         // Member
-        Route::get('/member/data', [MemberController::class, "data"])->name("member.data");
         Route::resource('/member', MemberController::class);
+        Route::get('/member/data', [MemberController::class, "data"])->name("member.data");
         Route::post('/member/delete-selected', [MemberController::class, "deleteSelected"])->name("member.deleteSelected");
         Route::post('/member/print-member', [MemberController::class, "printMember"])->name("member.printMember");
 
         // Supplier
-        Route::get('/supplier/data', [SupplierController::class, "data"])->name("supplier.data");
         Route::resource('/supplier', SupplierController::class);
+        Route::get('/supplier/data', [SupplierController::class, "data"])->name("supplier.data");
         Route::get('/suppliers/all', [SupplierController::class, 'getAll'])->name('suppliers.all');
         //Route::put('/supplier/{id}', [SupplierController::class, 'update'])->name('supplier.update');
 
@@ -76,15 +89,15 @@ Route::middleware([
         // GRN
         Route::get('/grn', [GrnController::class, 'index'])->name('grn.index');
         Route::get('/grn/create', [GrnController::class, 'create'])->name('grn.create');
-        Route::post('/grn', [GrnController::class, 'store'])->name('grn.store');
         Route::get('/grn/{grn}', [GrnController::class, 'show'])->name('grn.show');
         Route::get('/grn/{grn}/edit', [GrnController::class, 'edit'])->name('grn.edit');
+        Route::post('/grn', [GrnController::class, 'store'])->name('grn.store');
         Route::put('/grn/{grn}', [GrnController::class, 'update'])->name('grn.update');
         Route::delete('/grn/{grn}', [GrnController::class, 'destroy'])->name('grn.destroy');
 
         // Expense
-        Route::get('/expense/data', [ExpenseController::class, "data"])->name("expense.data");
         Route::resource('/expense', ExpenseController::class);
+        Route::get('/expense/data', [ExpenseController::class, "data"])->name("expense.data");
 
         // Sale
         Route::get('/sale/data', [SaleController::class, "data"])->name("sale.data");
@@ -98,30 +111,25 @@ Route::middleware([
 
         // Reports
         Route::get('/reports', [ReportsController::class, 'index'])->name('reports.index');
-            // Existing report route
-            Route::get('/reports', [ReportsController::class, 'index'])->name('reports.index');
-            // New route to handle report generation
-            Route::get('/reports/generate', [ReportsController::class, 'generate'])->name('reports.generate');
-            // Admin dashboard charts
-            Route::get('/reports/income-chart', function () {
+        // New route to handle report generation
+        Route::get('/reports/generate', [ReportsController::class, 'generate'])->name('reports.generate');
+        // Admin dashboard charts
+        Route::get('/reports/income-chart', function () {
                 return view('reports.income_chart');
             })->name('reports.income-chart');
-            Route::get('/reports/income-chart-data', [ReportsController::class, 'incomeChartData'])->name('reports.income-chart-data');
+        Route::get('/reports/income-chart-data', [ReportsController::class, 'incomeChartData'])->name('reports.income-chart-data');
 
-            //supplier_report
-            Route::get('/reports/supplier_report', [ReportsController::class, 'supplier_report'])->name('reports.supplier_report');
-            //supplier_pdf
-            Route::get('reports/supplier_exportPdf/{first_date?}/{last_date?}',[ReportsController::class, 'supplier_report_pdf'])->name('reports.supplier_exportPdf');
+        //supplier_report
+        Route::get('/reports/supplier_report', [ReportsController::class, 'supplier_report'])->name('reports.supplier_report');
+        Route::get('reports/supplier_exportPdf/{first_date?}/{last_date?}',[ReportsController::class, 'supplier_report_pdf'])->name('reports.supplier_exportPdf');
 
-            //product_report
-            Route::get('/reports/product_report', [ReportsController::class, 'product_report'])->name('reports.product_report');
-            //product_pdf
-            Route::get('reports/product_exportPdf', [ReportsController::class, 'product_exportPdf'])->name('reports.product_exportPdf');
+        //product_report
+        Route::get('/reports/product_report', [ReportsController::class, 'product_report'])->name('reports.product_report');
+        Route::get('reports/product_exportPdf', [ReportsController::class, 'product_exportPdf'])->name('reports.product_exportPdf');
 
-            //customer_report
-            Route::get('/reports/customer_report', [ReportsController::class, 'customer_report'])->name('reports.customer_report');
-             //customer_pdf
-            Route::get('reports/customer_exportPdf/{first_date?}/{last_date?}',[ReportsController::class, 'customer_exportPdf'])->name('reports.customer_exportPdf');
+        //customer_report
+        Route::get('/reports/customer_report', [ReportsController::class, 'customer_report'])->name('reports.customer_report');
+        Route::get('reports/customer_exportPdf/{first_date?}/{last_date?}',[ReportsController::class, 'customer_exportPdf'])->name('reports.customer_exportPdf');
 
         // User
         Route::get('/user/data', [UserController::class, "data"])->name("user.data");
@@ -132,11 +140,11 @@ Route::middleware([
 
         // Cashier
         Route::get('/cashier', [CashierController::class, "index"])->name("cashier.index");
-        Route::post('/cashier', [CashierController::class, "store"])->name("cashier.store");
         Route::get('/cashier/print/{sale}', [CashierController::class, 'print'])->name('cashier.print');
         Route::get('/cashier/drafts', [CashierController::class, 'getDraftSales'])->name('cashier.drafts');
         Route::get('/cashier/drafts/{id}', [CashierController::class, 'getDraftSale']);
         Route::get('/sales/customer/{id}', [SaleController::class, 'getCustomerSales'])->name('sales.customer');
+        Route::post('/cashier', [CashierController::class, "store"])->name("cashier.store");
         Route::post('/sales/return', [SaleController::class, 'storeReturn'])->name('sales.return');
 
         // Cashier Shift
@@ -146,6 +154,13 @@ Route::middleware([
         Route::post('/', [CashierShiftController::class, 'store'])->name('store');
         Route::post('/{id}/close', [CashierShiftController::class, 'close'])->name('close');
         Route::delete('/{id}', [CashierShiftController::class, 'destroy'])->name('destroy');
+        });
+
+        //Kitchen
+        Route::prefix('kitchen')->group(function () {
+        Route::get('/', [KitchenController::class, 'index'])->name('kitchen.index');
+        Route::get('/create', [KitchenController::class, 'create'])->name('kitchen.create');
+        Route::post('/store', [KitchenController::class, 'store'])->name('kitchen.store');
         });
 
     });
@@ -180,7 +195,6 @@ Route::middleware([
         });
     });
 
-
     // Transaction
     Route::get('/transaction/new', [SaleController::class, "create"])->name("transaction.new");
     Route::post('/sale/store', [SaleController::class, 'store'])->name('sale.store');
@@ -195,3 +209,4 @@ Route::middleware([
     Route::get('/profile', [UserController::class, "profile"])->name("user.profile");
     Route::put('/profile', [UserController::class, "updateProfile"])->name("user.updateProfile");
 });
+
